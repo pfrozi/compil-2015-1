a4282ee3379db91290d311f42932ff4bce3307b2
pfrozi <pedrohfcs@gmail.com>
2015-05-21 20:04:46 +0000

    Fix cc_regs.c errors

--- src/cc_regs.c
+++ src/cc_regs.c
@@ -8,12 +8,17 @@ void  init_cc_regs(){
 char* get_reg(){
     
     char*   new_reg;
-    char[2] buff1 = "r";
-    char[4] buff2;
     
+    char    buff1[6];
+    char    buff2[4];
+
+    new_reg = (char*)malloc(sizeof(buff1));
+    
+    strncpy( buff1, "r", 6 );
     itoa(id, buff2, 10);
-    
     strncat(buff1, buff2, 6);
+
+    strncpy( new_reg, buff1, 6);
     
     id++;
     
