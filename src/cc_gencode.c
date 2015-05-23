3c23ff70f3cde74bcf3d9caeb263d24b1335f305
Wladimir Leuschner <wladimir_leuschner@web.de>
2015-05-23 00:50:44 -0300

    Literal Code Gen

--- src/cc_gencode.c
+++ src/cc_gencode.c
@@ -1,2 +1,11 @@
 #include "cc_gencode.h" 
 
+void literal(comp_tree_t* t)
+{
+    if(t->item->type==IKS_INT)
+    {
+        int val = t->item->sentry->val_int;
+        char* reg1 = (char*)malloc(sizeof(char)*8);
+        get_iloc_code(OP_LOADI,reg1, NULL, get_reg());
+    }
+}
