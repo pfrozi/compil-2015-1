ea696a1bc2d622d513b533f04cd90f64acf0b4d8
Wladimir Leuschner <wladimir_leuschner@web.de>
2015-05-23 02:44:43 -0300

    Created Stubs

--- src/cc_gencode.c
+++ src/cc_gencode.c
@@ -55,6 +55,46 @@ void gen_div(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
     t1->item->codes[t1->item->num_codes] = get_iloc_code(OP_DIV,t2->item->result,t3->item->result,result);
 }
 
+void gen_and(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+    char* result = get_reg();
+    t1->item->num_codes = t2->item->num_codes + t3->item->num_codes + 1;
+    t1->item->codes = (iloc_code_t**)malloc(sizeof(iloc_code_t*)*t1->item->num_codes);
+    memcpy(t1->item->codes,t2->item->codes,t2->item->num_codes);
+    memcpy(t1->item->codes+t2->item->num_codes,t3->item->codes,t3->item->num_codes);
+    t1->item->codes[t1->item->num_codes] = get_iloc_code(OP_AND,t2->item->result,t3->item->result,result);
+}
+
+void gen_or(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+    char* result = get_reg();
+    t1->item->num_codes = t2->item->num_codes + t3->item->num_codes + 1;
+    t1->item->codes = (iloc_code_t**)malloc(sizeof(iloc_code_t*)*t1->item->num_codes);
+    memcpy(t1->item->codes,t2->item->codes,t2->item->num_codes);
+    memcpy(t1->item->codes+t2->item->num_codes,t3->item->codes,t3->item->num_codes);
+    t1->item->codes[t1->item->num_codes] = get_iloc_code(OP_OR,t2->item->result,t3->item->result,result);
+}
+
+void gen_log_and(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+}
+
+void gen_log_or(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+}
+
+void gen_if_else(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+}
+
+void gen_do_while(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+}
+
+void gen_while_do(comp_tree_t* t1, comp_tree_t* t2, comp_tree_t* t3)
+{
+}
+
 void gen_int_invert(comp_tree_t* t1)
 {
 }
